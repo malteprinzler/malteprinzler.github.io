ad02b7216771de047cf8c5d5933542cdbbf8f855
mprinzler <malte.prinzler@web.de>
2024-09-19 15:47:53 +0200

    First Version of Joker Webpage

---
+++ projects/joker/static/js/bulma_carousel.js
@@ -0,0 +1,35 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("item-slide");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+}
+
+bulmaCarousel.attach('#results-carousel', {
+            slidesToScroll: 1,
+            slidesToShow: 1,
+            infinite: true,
+            autoplay: false,
+            pagination: false,
+        });
+bulmaCarousel.attach('#results-carousel-3', {
+            slidesToScroll: 1,
+            slidesToShow: 3,
+            infinite: true,
+            autoplay: false,
+        });
